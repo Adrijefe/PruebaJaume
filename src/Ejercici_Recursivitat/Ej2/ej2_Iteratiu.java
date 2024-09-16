702ba1a7934c6885917f8b642be1d8739398aa41
adrpeiser <serrapeiroa@gmail.com>
2024-09-16 12:08:07 +0200

    ejercicios Recursivitat

--- src/Ejercici_Recursivitat/Ej2/ej2_Iteratiu.java
+++ src/Ejercici_Recursivitat/Ej2/ej2_Iteratiu.java
@@ -3,7 +3,16 @@ package Ejercici_Recursivitat.Ej2;
 public class ej2_Iteratiu {
     public static void main(String[] args) {
         int vector[] = new int[5];
-        vector
+        vector[0] = 10;
+        vector[1] = 20;
+        vector[2] = 30;
+        vector[3] = 40;
+        vector[4] = 50;
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i]);
+
+        }
+
 
 
     }
